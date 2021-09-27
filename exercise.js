c44c0692b0c7e75d7bb38e20d9630f099c1644d8
Sergey <lolka026@gmail.com>
2021-09-27 19:32:26 +0300

    dz

--- exercise.js
+++ exercise.js
@@ -2,23 +2,23 @@ let str = "69Word"
 let bool = false
 let num = 1
 
-// console.log(str + bool); - boolean переходит в значение string - Wordfalse
-// console.log(str + num); - number переходит в значение string - 69Word1
-// console.log(num + bool); - boolean переходит в значение number, false=>0, 1+0=1
+ console.log(str + bool); //- boolean переходит в значение string - Wordfalse
+ console.log(str + num); // - number переходит в значение string - 69Word1
+ console.log(num + bool); // - boolean переходит в значение number, false=>0, 1+0=1
 
-// console.log(str * bool); - возвращает значение NaN
-// console.log(str * num);  - возвращает значение NaN
-// console.log(num * bool); - возваращает ноль, false => 0, 1 * 0 = 0
-
-
-// console.log(str - bool); - возвращает значение NaN
-// console.log(str - num);  - возвращает значение NaN
-// console.log(num - bool);  - возваращает ноль, false => 0, 1 - 0 = 1
+ console.log(str * bool); // - возвращает значение NaN
+ console.log(str * num);  // - возвращает значение NaN
+ console.log(num * bool); // - возваращает ноль, false => 0, 1 * 0 = 0
 
 
-// console.log(str / bool); - возвращает значение NaN
-// console.log(str / num);  - возвращает значение NaN
-// console.log(num / bool); - возваращает infinity, false => 0, 1 / 0 = infinity
+ console.log(str - bool);  // - возвращает значение NaN
+ console.log(str - num);  // - возвращает значение NaN
+ console.log(num - bool); // - возваращает ноль, false => 0, 1 - 0 = 1
+
+
+ console.log(str / bool); // - возвращает значение NaN
+ console.log(str / num);  //  - возвращает значение NaN
+ console.log(num / bool); // - возваращает infinity, false => 0, 1 / 0 = infinity
 
   str = parseInt(str);
   console.log(typeof str);
