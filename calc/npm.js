3ca04eec1ba21eaba60630512d36f532770b6600
Sergey <lolka026@gmail.com>
2021-10-14 16:22:58 +0300

    dzzzz

---
+++ calc/npm.js
@@ -0,0 +1,60 @@
+const fs = require('fs-extra');
+const src = '/Users/pro/lol/calc/papka/a.txt';
+const dest = '/Users/pro/lol/calc/papka2/b.txt';
+
+
+function foo() {
+    fs.mkdir('papka', err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    })
+
+    fs.writeFile(src, 'a', err => {
+        if (err) {
+            console.log(err)
+            return
+        }
+    })
+
+    fs.mkdir('papka2', err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    })
+
+    fs.move(src, dest, err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    });
+
+    setTimeout(() => fs.remove(dest, err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    }), 500);
+   
+    setTimeout(() => fs.remove('papka', err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    }), 600);
+
+    setTimeout(() => fs.remove('papka2', err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+    }), 600);
+
+   
+}
+
+foo();
+
